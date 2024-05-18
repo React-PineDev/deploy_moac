8bab79b3c5d90916bcae465de0fd69a9ab6b6815
Pine1611 <iampine1611@gmail.com>
2024-05-18 09:05:42 +0700

    update path

---
+++ assets/ListContent-UXdB8Csd.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DzJf_Pg9.js";const m=({className:s,listData:n})=>{const r=n.map((e,i)=>t.jsx("li",{children:e},i));return t.jsx(t.Fragment,{children:t.jsx("ul",{className:s,children:r})})};export{m as L};
