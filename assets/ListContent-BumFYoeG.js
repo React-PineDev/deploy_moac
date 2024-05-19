8aa055e8fccb72fe4045e926fb8fb669dcb50372
Pine1611 <iampine1611@gmail.com>
2024-05-19 09:41:06 +0700

    test new updates build

--- assets/ListContent-BumFYoeG.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-mBkGTe8k.js";const m=({className:s,listData:n})=>{const r=n.map((e,i)=>t.jsx("li",{children:e},i));return t.jsx(t.Fragment,{children:t.jsx("ul",{className:s,children:r})})};export{m as L};
